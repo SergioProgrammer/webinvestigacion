3e3fdf5856fbbe70b6e53a9743b87710deed52ca
Sergio <sergiosanpacheco@gmail.com>
2025-11-13 10:14:31 +0100

    redeploy_astroconfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel';
 
-// https://astro.build/config
 export default defineConfig({
 	output: 'server',
-	adapter: node({
-		mode: 'standalone',
-	}),
+	adapter: vercel(),
 	integrations: [react(), tailwind()],
 });
